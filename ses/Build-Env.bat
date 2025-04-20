9fca1f62818975772b6c76760883ecb5bd3bb734
Peter Dodd <pdodd@digi.com>
2025-04-20 11:22:02 -0400

    Added batchfiles to build and run the test environments

---
+++ ses/Build-Env.bat
@@ -0,0 +1,1 @@
+docker build -t unit-tests-image -f Dockerfile .
---
+++ ses/Run-tests.bat
@@ -0,0 +1,2 @@
+docker run --rm -v "D:/Projects/ScalyCare/ThermoChirp/MCP9808/ses/src:/project/src" -v "D:/Projects/ScalyCare/ThermoChirp/MCP9808/ses/tests:/project/tests" unit-tests-image
+pause
